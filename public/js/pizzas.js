8f5216b9310b8e8585d322cbfecad809ac7af53b
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2021-11-04 21:04:45 -0300

    master

--- public/js/pizzas.js
+++ public/js/pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Muçarela', img:'/public/images/pizza.png', sizes:['100g', '530g', '860g'], sizesPrice:["R$ 10,19", "R$ 16.19", "R$ 20.19"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'/public/images/pizza2.png', sizes:['320g', '530g', '860g'], sizesPrice:["R$ 8,00", "R$ 14.00", "R$ 18.00"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'/public/images/pizza3.png', sizes:['320g', '530g', '860g'], sizesPrice:["R$ 7,50", "R$ 13.50", "R$ 17.50"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Portuguesa', img:'/public/images/pizza4.png', sizes:['320g', '530g', '860g'], sizesPrice:["R$ 9,99", "R$ 15.99", "R$ 19.99"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'À Moda da Casa', img:'/public/images/pizza6.png', sizes:['320g', '530g', '860g'], sizesPrice:["R$ 8,50", "R$ 14.50", "R$ 18.50"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Sorvete', img:'/public/images/pizza5.png', sizes:['320g', '530g', '860g'], sizesPrice:["R$ 15,99", "R$ 23.99", "R$ 31.99"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'/public/images/pizza7.png', sizes:['320g', '530g', '860g'], sizesPrice:["R$ 16,99", "R$ 24,99", "R$ 32,99"], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Muçarela', img:'/public/images/pizza.png', sizes:['100g', '530g', '860g'], sizesPrice:[10.19, 16.19, 20.19], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'/public/images/pizza2.png', sizes:['320g', '530g', '860g'], sizesPrice:[8.00, 14.00, 18.00], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'/public/images/pizza3.png', sizes:['320g', '530g', '860g'], sizesPrice:[7.50, 13.50, 17.50], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Portuguesa', img:'/public/images/pizza4.png', sizes:['320g', '530g', '860g'], sizesPrice:[9.99, 15.99, 19.99], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'À Moda da Casa', img:'/public/images/pizza6.png', sizes:['320g', '530g', '860g'], sizesPrice:[8.50, 14.50, 18.50], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Sorvete', img:'/public/images/pizza5.png', sizes:['320g', '530g', '860g'], sizesPrice:[15.99, 23.99, 31.99], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'/public/images/pizza7.png', sizes:['320g', '530g', '860g'], sizesPrice:[16.99, 24.99, 32.99], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
--- public/js/script.js
+++ public/js/script.js
@@ -12,9 +12,11 @@ function elem(item) {
 let cartItemQtde = elem(".cart--item--qt");
 let cartItemMenos = elem(".cart--item-qtmenos");
 let cartItemMais = elem(".cart--item-qtmais");
-let pizzaItemQtde = elem(".pizzaInfo--qt");
-let pizzaItemMenos = elem(".pizzaInfo--qtmenos");
+let PIZZA_ITEM_QTDE = elem(".pizzaInfo--qt");
+let PIZZA_ITEM_MENOS = elem(".pizzaInfo--qtmenos");
 let pizzaItemMais = elem(".pizzaInfo--qtmais");
+let PIZZA_ID = 0;
+let PIZZA_VALOR = 0;
 
 // FIM DECLARAÇÃO VARIÁVEIS
 
@@ -30,7 +32,11 @@ pizzaJson.map((item) => {
     pizzaItens.querySelector(".pizza-item a").setAttribute("data-id", item.id);
     pizzaItens.querySelector(".pizza-item a").href = "#";
     pizzaItens.querySelector(".pizza-item--img img").src = item.img;
-    pizzaItens.querySelector(".pizza-item--price").innerHTML = item.sizesPrice[2];
+    pizzaItens.querySelector(".pizza-item--price").innerHTML = item.sizesPrice[2].toLocaleString("pt-BR", {
+        style: "currency", 
+        currency: "BRL",
+        maximumFractionDigits: 2
+    });
     pizzaItens.querySelector(".pizza-item--name").innerHTML = item.name;
     pizzaItens.querySelector(".pizza-item--desc").innerHTML = item.description;
 
@@ -48,7 +54,10 @@ pizzaJson.map((item) => {
 function modal(id) {
     let modal = elem(".pizzaWindowArea");
     let modalItem = [];
+    
     id = Number(id.getAttribute("data-id"));
+
+    PIZZA_ID = id;
 
     modalItem = pizzaJson.filter((item) => {
         return item.id == id;
@@ -57,41 +66,92 @@ function modal(id) {
     modal.querySelector(".pizzaBig img").src = modalItem[0].img;
     modal.querySelector(".pizzaInfo h1").innerHTML = modalItem[0].name;
     modal.querySelector(".pizzaInfo--desc").innerHTML = modalItem[0].description;
+
     modal.querySelectorAll(".pizzaInfo--size").forEach((item, index) => {
-        item.querySelector("span").innerHTML = modalItem[0].sizesPrice[index];
-    })
+        item.querySelector("span").innerHTML = modalItem[0].sizesPrice[index].toLocaleString("pt-BR", {
+            style: "currency", 
+            currency: "BRL", 
+            maximumFractionDigits: 2
+        });
+    });
+
+    PIZZA_VALOR = modal.querySelector(".pizzaInfo--pricearea .pizzaInfo--actualPrice").innerHTML = modalItem[0].sizesPrice[2];
+
+    modal.querySelector(".pizzaInfo--pricearea .pizzaInfo--actualPrice").innerHTML = modalItem[0].sizesPrice[2].toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        maximumFractionDigits: 2
+    });
     
     modal.style.display = "flex";
     modal.style.zIndex = 1000;
-
 };
+
+
 
 // FIM PREENCHIMENTO MODAL
 
 // INÍCIO FECHA MODAL
 
-function fechaModal() {
+function fechaModal() { 
     let modal = elem(".pizzaWindowArea");
+    let pizzaValor = elem(".pizzaWindowArea .pizzaInfo--pricearea .pizzaInfo--actualPrice");
 
+    PIZZA_ITEM_QTDE.innerHTML = 1;
     modal.style.display = "none";
     modal.style.zIndex = "-1";
 }
 
 // FIM FECHA MODAL
 
+
 // INÍCIO + E - QUANTIDADE DE PRODUTO
 
-pizzaItemMenos.addEventListener("click", () => {
-    if(Number(pizzaItemQtde.innerHTML) > 0) {
-        pizzaItemQtde.innerHTML = Number(pizzaItemQtde.innerHTML) - 1;
-    } else {
-        pizzaItemQtde.innerHTML = 0;
-    }
+PIZZA_ITEM_MENOS.addEventListener("click", () => {
+    let pizzaValor = elem(".pizzaWindowArea .pizzaInfo--pricearea .pizzaInfo--actualPrice");
+    let pizzaTamanhoSelected = elem(".pizzaInfo--sizes .selected");
+    let json = pizzaJson;
 
+    pizzaTamanhoSelected = pizzaTamanhoSelected.getAttribute("data-key");
+    pizzaTamanhoSelected = Number(pizzaTamanhoSelected);
+    pizzaTamSelValor = json[PIZZA_ID - 1].sizesPrice[pizzaTamanhoSelected];
+
+    if(Number(PIZZA_ITEM_QTDE.innerHTML) > 0) {
+        PIZZA_ITEM_QTDE.innerHTML = Number(PIZZA_ITEM_QTDE.innerHTML) - 1;
+        pizzaValor.innerHTML = Number(PIZZA_ITEM_QTDE.innerHTML) * Number(pizzaTamSelValor);
+
+        PIZZA_VALOR = Number(pizzaValor.innerHTML);
+
+        pizzaValor.innerHTML = Number(pizzaValor.innerHTML).toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+            maximumFractionDigits: 2
+        });
+    } else {
+        PIZZA_ITEM_QTDE.innerHTML = 0;
+    }
+    qtde = Number(PIZZA_ITEM_QTDE.innerHTML);
 });
 
 pizzaItemMais.addEventListener("click", () => {
-    pizzaItemQtde.innerHTML = Number(pizzaItemQtde.innerHTML) + 1;
+    let pizzaValor = elem(".pizzaWindowArea .pizzaInfo--pricearea .pizzaInfo--actualPrice");
+    let pizzaTamanhoSelected = elem(".pizzaInfo--sizes .selected");
+    let json = pizzaJson;
+
+    pizzaTamanhoSelected = pizzaTamanhoSelected.getAttribute("data-key");
+    pizzaTamanhoSelected = Number(pizzaTamanhoSelected);
+    pizzaTamSelValor = json[PIZZA_ID - 1].sizesPrice[pizzaTamanhoSelected];
+
+    PIZZA_ITEM_QTDE.innerHTML = Number(PIZZA_ITEM_QTDE.innerHTML) + 1;
+    pizzaValor.innerHTML = Number(PIZZA_ITEM_QTDE.innerHTML) * Number(pizzaTamSelValor);
+
+    PIZZA_VALOR = Number(pizzaValor.innerHTML);
+
+    pizzaValor.innerHTML = Number(pizzaValor.innerHTML).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        maximumFractionDigits: 2
+    });
 });
 
 // FIM + E - QUANTIDADE DE PRODUTO
